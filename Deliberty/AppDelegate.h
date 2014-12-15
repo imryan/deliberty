0b7c1d42292cdfc80260156fa55ab9a93f29c4a6
Ryan Cohen <notryancohen@gmail.com>
2014-12-15 13:59:54 -0500

    Initial Commit

---
+++ Deliberty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deliberty
+//
+//  Created by Ryan Cohen on 12/15/14.
+//  Copyright (c) 2014 Ryan Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Deliberty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deliberty
+//
+//  Created by Ryan Cohen on 12/15/14.
+//  Copyright (c) 2014 Ryan Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
